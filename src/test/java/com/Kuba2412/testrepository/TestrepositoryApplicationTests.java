6c68a67b7c0f77731de56c61ef2bdfe36784acbf
Kuba2412 <xkubusx5@gmail.com>
2024-10-07 13:06:28 +0200

    commit message

---
+++ src/test/java/com/Kuba2412/testrepository/TestrepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kuba2412.testrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestrepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
